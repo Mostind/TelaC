57dc1865c2c182e7fabf7ba7427deace28c56dc8
Mostind <joaopedroalmeida007@gmail.com>
2023-05-01 07:05:51 -0300

    atualizacao1

---
+++ tabelaC.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Tabela</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel="stylesheet" href="tabelaC.css">
+
+</head>
+<body>
+  <main>
+      
+        <!-- Os botões que ficam dentro de título -->
+        <div class="tituloBot">
+            <a href="" class="botTit">Voltar</a>
+            <h3>PRODUTOS</h3>
+            <a href="" class="botTit">Novo Conta</a>
+        </div>
+      
+         <!--A primeira parte da tabela-->
+      <div class="grupo">
+            <div class="sub-grupo">IDCONTA</div>
+            <div class="sub-grupo">GARÇOM</div>
+            <div class="sub-grupo">DATA ABERTURA</div>
+            <div class="sub-grupo">HORA ABERTURA</div>
+            <div class="sub-grupo">PRODUTO</div>
+            <div class="sub-grupo">QUANT.</div>
+            <div class="sub-grupo">VALOR TOTAL</div>
+      </div>
+           <!-- A segunda parte da tabela  -->
+           <div class="grupo2">
+               <div class="sub-grupo2">1</div>
+               <div class="sub-grupo2">JOÃO PEDRO DE ALMEIDA</div>
+               <div class="sub-grupo2">01/05/2023</div>
+               <div class="sub-grupo2">06:56:51</div>
+               <div class="sub-grupo2">Pão</div>
+               <div class="sub-grupo2">125</div>
+               <div class="sub-grupo2">R$312,50</div>
+               <!-- Botões dentro da tabela -->
+               <div class="sub-grupo2"><input type="submit" value="Enviar"> </div>
+               <div class="sub-grupo2"><input type="submit" value="Deletar"></div>
+           </div>
+           
+           <div class="grupo2">
+            <div class="sub-grupo2">2</div>
+            <div class="sub-grupo2">Alberto Ferraira de Sousa</div>
+            <div class="sub-grupo2">01/05/2023</div>
+            <div class="sub-grupo2">06:56:51</div>
+            <div class="sub-grupo2">Pão</div>
+            <div class="sub-grupo2">125</div>
+            <div class="sub-grupo2">R$312,50</div>
+            <!-- Botões dentro da tabela -->
+            <div class="sub-grupo2"><input type="submit" value="Enviar"> </div>
+            <div class="sub-grupo2"><input type="submit" value="Deletar"></div>
+        </div>
+        
+        <div class="grupo2">
+            <div class="sub-grupo2">1</div>
+            <div class="sub-grupo2">JOÃO PEDRO DE ALMEIDA</div>
+            <div class="sub-grupo2">01/05/2023</div>
+            <div class="sub-grupo2">06:56:51</div>
+            <div class="sub-grupo2">Pão</div>
+            <div class="sub-grupo2">125</div>
+            <div class="sub-grupo2">R$312,50</div>
+            <!-- Botões dentro da tabela -->
+            <div class="sub-grupo2"><input type="submit" value="Enviar"> </div>
+            <div class="sub-grupo2"><input type="submit" value="Deletar"></div>
+        </div>
+        
+        <div class="grupo2">
+            <div class="sub-grupo2">1</div>
+            <div class="sub-grupo2">JOÃO PEDRO DE ALMEIDA</div>
+            <div class="sub-grupo2">01/05/2023</div>
+            <div class="sub-grupo2">06:56:51</div>
+            <div class="sub-grupo2">Pão</div>
+            <div class="sub-grupo2">125</div>
+            <div class="sub-grupo2">R$312,50</div>
+            <!-- Botões dentro da tabela -->
+            <div class="sub-grupo2"><input type="submit" value="Enviar"> </div>
+            <div class="sub-grupo2"><input type="submit" value="Deletar"></div>
+        </div>
+        
+        </div>
+  </main>
+
+</body>
+</html>
